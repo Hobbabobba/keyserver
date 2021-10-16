ee25d24d186ff35864114de19bd62a4481d2d774
Hobbabobba <Hobbabobba@users.noreply.github.com>
2021-10-16 11:36:50 +0200

    wget installieren

--- einrichtung_client.sh
+++ einrichtung_client.sh
@@ -1,5 +1,5 @@
 !/bin/bash
-apt install curl
+apt install curl wget
 echo -e "PubkeyAuthentication yes \nPasswordAuthentication yes \nAuthorizedKeysCommand /usr/local/bin/userkeys.sh \nAuthorizedKeysCommandUser nobody" >> /etc/ssh/sshd_config
 chmod a+x /usr/local/bin/userkeys.sh
 service ssh restart
