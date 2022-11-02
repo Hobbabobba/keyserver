69338baf487e5b2b00bf645208cf2a8035deb274
Hobbabobba <Hobbabobba@users.noreply.github.com>
2022-11-02 14:09:41 +0100

    alte RSA Keys akzeptieren

--- einrichtung_client.sh
+++ einrichtung_client.sh
@@ -1,6 +1,6 @@
 !/bin/bash
 apt install curl wget
-echo -e "PubkeyAuthentication yes \nPasswordAuthentication yes \nAuthorizedKeysCommand /usr/local/bin/userkeys.sh \nAuthorizedKeysCommandUser nobody" >> /etc/ssh/sshd_config
+echo -e "PubkeyAuthentication yes \nPasswordAuthentication yes \nAuthorizedKeysCommand /usr/local/bin/userkeys.sh \nAuthorizedKeysCommandUser nobody\nPubkeyAcceptedKeyTypes +ssh-rsa\nHostKeyAlgorithms +ssh-rsa" >> /etc/ssh/sshd_config
 wget -P /usr/local/bin https://raw.githubusercontent.com/Hobbabobba/keyserver/main/userkeys.sh
 chmod a+x /usr/local/bin/userkeys.sh
 service ssh restart
