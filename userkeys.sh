e9df517e68d4fdc05e5d62410de07e431275a95c
Hobbabobba <Hobbabobba@users.noreply.github.com>
2025-09-16 09:10:48 +0200

    Update userkeys.sh

--- userkeys.sh
+++ userkeys.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-curl -sf https://raw.githubusercontent.com/Hobbabobba/keyserver/refs/heads/main/$1/keys
+curl -sf https://git.nauheimtech.de/StefanMewes/keyserver/raw/branch/main/$1/keys
 if [ $? == 0 ]; then
   exit 0
 else 
-  curl -sf https://git.nauheimtech.de/StefanMewes/keyserver/raw/branch/main/$1/keys
+  curl -sf https://raw.githubusercontent.com/Hobbabobba/keyserver/refs/heads/main/$1/keys
 fi
