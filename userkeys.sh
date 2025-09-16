13b19e9f09cdd304fe75bc06557d2ec25635330d
Hobbabobba <Hobbabobba@users.noreply.github.com>
2025-09-16 11:48:41 +0200

    Update userkeys.sh

--- userkeys.sh
+++ userkeys.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-curl -sf https://git.nauheimtech.de/StefanMewes/keyserver/raw/branch/main/$1/keys
-if [ $? == 0 ]; then
-  exit 0
-else 
-  curl -sf https://raw.githubusercontent.com/Hobbabobba/keyserver/refs/heads/main/$1/keys
-fi
+
+# Zeitlimits für curl (Sekunden)
+CONNECT_TIMEOUT=2
+MAX_TIME=2
+
+# Erste URL versuchen
+curl -sf --connect-timeout $CONNECT_TIMEOUT --max-time $MAX_TIME \
+    https://raw.githubusercontent.com/Hobbabobba/keyserver/refs/heads/main/$1/keys && exit 0
+
+# Falls die erste URL fehlschlägt, zweite URL versuchen
+curl -sf --connect-timeout $CONNECT_TIMEOUT --max-time $MAX_TIME \
+    https://git.nauheimtech.de/StefanMewes/keyserver/raw/branch/main/$1/keys
